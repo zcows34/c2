a7ea942528b0364a8f619d10cb0a319aa24716f3
zcows34 <165367233+zcows34@users.noreply.github.com>
2024-07-07 21:12:25 +1200

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const { exec } = require('child_process');
 
 app.use(express.static('public'));
 
-app.get('/cowsay/:message', (req, res) => {
-  exec(`./cowsay ${req.params.message}`, {timeout: 5000}, (error, stdout) => {
+app.get('/', (req, res) => {
+  exec(`./cowsay ${req.req.query.id}`, {timeout: 5000}, (error, stdout) => {
     if (error) return res.status(500).end();
     res.type('txt').send(stdout).end();
   });
